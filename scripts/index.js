99d7827256ffbe467780f3fe77df68977674d3c1
iulianstan <iulian1226@gmail.com>
2021-04-26 11:54:48 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 // Any code you will write for your website (in the future!) should go here
+// Add a to do task function
+const addTask = () => {
+  let newTask = document.querySelector("#newTask").value;
+  if (newTask != "") {
+    let newLi = document.createElement("li");
+    let text = document.createTextNode(newTask);
+    newLi.appendChild(text);
+    document.querySelector(".result").appendChild(newLi);
+    document.querySelector("#newTask").value = "";
+  } else {
+    document.querySelector("#newTask").placeholder = "Please add text";
+  }
+};
